e511f9adb9d3509623447ea9c7d269a6ffe5a9ae
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-07 14:17:07 -0400

    implement the benchmark method

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,10 @@
+# A benchmark in programming is a method of measuring the performance of code by recording how long it takes to execute. It's used to test the speed or efficiency of a particular operation, algorithm, or code block.
+
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield # Execute the block of code passed to the method
+  end_time = Time.now
+  return end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
